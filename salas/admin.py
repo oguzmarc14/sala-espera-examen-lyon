a39cac0577af4cee04329aa187b4508ce9000c7a
oguzmarc14 <oguzmarc14@gmail.com>
2025-08-14 15:04:30 -0600

    chore(admin.py): registrar modelo Sala em el admin de Django

--- salas/admin.py
+++ salas/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Sala 
 
 # Register your models here.
+@admin.site.register(Sala)
+class SalaAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'capacidad','ubicacion','created_at','updated_at')
+    search_fields = ('nombre','ubicacion')
+    list_filter = ('capacidad', 'ubicacion')
