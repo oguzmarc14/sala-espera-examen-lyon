28130391ff7f8a50d90765022d7aac98d06074da
oguzmarc14 <oguzmarc14@gmail.com>
2025-08-14 14:30:48 -0600

    Creacion de modelo Sala con campos y metodo __str__

--- salas/models.py
+++ salas/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
+class Sala(models.Model):
+    nombre = models.CharField(max_length=100, unique=True)
+    capacidad = models.PositiveBigIntegerField(validators=[MinValueValidator(1)])
+    ubicacion = models.CharField(max_length=200, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.nombre
 # Create your models here.
