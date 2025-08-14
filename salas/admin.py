2f81bea8a9d00827bea9809560a68bb7f295314a
oguzmarc14 <oguzmarc14@gmail.com>
2025-08-14 17:22:32 -0600

    fix(settings.py): coma en INSTALED_APPS y registro de modelo Sala en admin

--- salas/admin.py
+++ salas/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Sala 
 
 # Register your models here.
-@admin.site.register(Sala)
+@admin.register(Sala)
 class SalaAdmin(admin.ModelAdmin):
     list_display = ('nombre', 'capacidad','ubicacion','created_at','updated_at')
     search_fields = ('nombre','ubicacion')
