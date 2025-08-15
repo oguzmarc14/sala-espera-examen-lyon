e7d07a7d7b0793be388aa71bf41e740cd9553fef
oguzmarc14 <oguzmarc14@gmail.com>
2025-08-15 13:16:17 -0600

    fix(models.py): mover class Meta dentro de Reserva

--- salas/models.py
+++ salas/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.validators import MinValueValidator
 
+
 class Sala(models.Model):
     nombre = models.CharField(max_length=100, unique=True)
     capacidad = models.PositiveBigIntegerField(validators=[MinValueValidator(1)])
@@ -10,20 +11,32 @@ class Sala(models.Model):
 
     def __str__(self):
         return self.nombre
+
+
 # Create your models here.
 
+
 class Reserva(models.Model):
-    sala = models.ForeignKey('Sala', on_delete=models.CASCADE, related_name='reservas')
+    sala = models.ForeignKey("Sala", on_delete=models.CASCADE, related_name="reservas")
     titulo = models.CharField(max_length=100)
     nombre_contacto = models.CharField(max_length=100)
     email_contacto = models.EmailField()
     fecha = models.DateField()
     hora_inicio = models.TimeField()
     hora_fin = models.TimeField()
-    estado = models.CharField(max_length=20, choices=[('pendiente','Pendiente'), ('confirmada', 'Confirmada'), ('cancelada','Cancelada')], default='pendiente')
+    estado = models.CharField(
+        max_length=20,
+        choices=[
+            ("pendiente", "Pendiente"),
+            ("confirmada", "Confirmada"),
+            ("cancelada", "Cancelada"),
+        ],
+        default="pendiente",
+    )
     notas = models.TextField(blank=True)
     creado_en = models.DateTimeField(auto_now_add=True)
     actualizado_en = models.DateTimeField(auto_now=True)
 
-    ordering = ['fecha','hora_inicio']
-    indexes = [models.Index(fields=['sala','fecha'])]
+    class Meta:
+        ordering = ["fecha", "hora_inicio"]
+        indexes = [models.Index(fields=["sala", "fecha"])]
