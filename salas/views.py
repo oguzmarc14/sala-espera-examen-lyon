4eb40284cecb4b722048682911c07d4870cf8fdf
oguzmarc14 <oguzmarc14@gmail.com>
2025-08-14 18:06:20 -0600

    feat(views.py): viewset CRUD para Sala

--- salas/views.py
+++ salas/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets 
+from .models import Sala
+from .serializers import SalaSerializer
 
 # Create your views here.
+class SalaViewSet(viewsets.ModelViewSet):
+    queryset = Sala.objects.all().order_by("nombre")
+    serializer_class = SalaSerializer
