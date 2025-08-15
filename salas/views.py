d25c1e94177fc65fb6129a42a36f11a4ab3529d1
oguzmarc14 <oguzmarc14@gmail.com>
2025-08-15 14:36:55 -0600

    fix(migrations/): cambio de nombres en tabla salas

--- salas/views.py
+++ salas/views.py
@@ -1,8 +1,16 @@
 from rest_framework import viewsets 
 from .models import Sala
-from .serializers import SalaSerializer
+from .serializers import SalaSerializer, ReservaSerializer
+from rest_framework.permissions import AllowAny
 
 # Create your views here.
 class SalaViewSet(viewsets.ModelViewSet):
     queryset = Sala.objects.all().order_by("nombre")
     serializer_class = SalaSerializer
+    permission_classes = [AllowAny]
+    
+class ReservaViewSet(viewsets.ModelViewSet):
+    queryset = Sala.objects.all()
+    serializer_class = SalaSerializer
+    permission_classes = [AllowAny]
+    
