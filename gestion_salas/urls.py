8b3870710206e03a0b67e40a1da22aaf6161fffa
oguzmarc14 <oguzmarc14@gmail.com>
2025-08-18 01:22:54 -0600

    docs(READNE): uso de la API, reglas y como correr tests

--- gestion_salas/urls.py
+++ gestion_salas/urls.py
@@ -1,30 +1,13 @@
-"""
-URL configuration for gestion_salas project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from salas.views import SalaViewSet, ReservaViewSet
 
 router = DefaultRouter()
-router.register(r"salas", SalaViewSet, basename="salas")
-router.register(r"reservas", ReservaViewSet, basename="reservas")
-                
+router.register(r"salas", SalaViewSet, basename="sala")
+router.register(r"reservas", ReservaViewSet, basename="reserva")
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path ("api/", include(router.urls)),
+    path("admin/", admin.site.urls),
+    path("api/", include(router.urls)),
 ]
-    
