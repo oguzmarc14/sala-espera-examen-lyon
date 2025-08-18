d40e775a1194452fdb24f8251c398b01d2c2d823
oguzmarc14 <oguzmarc14@gmail.com>
2025-08-18 02:59:29 -0600

    feat(admin): registrar modelo Sala y Reserva en el panel de administracion

--- salas/admin.py
+++ salas/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import Sala 
+from .models import Sala
+
 
 # Register your models here.
 @admin.register(Sala)
 class SalaAdmin(admin.ModelAdmin):
-    list_display = ('nombre', 'capacidad','ubicacion','creado_el','actualizado_el')
-    search_fields = ('nombre','ubicacion')
-    list_filter = ('capacidad', 'ubicacion')
+    list_display = ("nombre", "capacidad", "ubicacion", "creado_el", "actualizado_el")
+    search_fields = ("nombre", "ubicacion")
+    list_filter = ("capacidad", "ubicacion")
